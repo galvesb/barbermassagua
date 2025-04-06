769310a567d3050d72ad349a088e5d460634997d
galvesb <guilherme.barrozzo@gmail.com>
2025-04-05 21:37:49 -0300

    centralizando mobile

--- app/page.tsx
+++ app/page.tsx
@@ -321,8 +321,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-[#2a2a38] flex items-center justify-center p-4">
-      {/* altura */}
-      <div className="w-full max-w-sm h-[80vh] bg-[#1f1f29] rounded-3xl p-6 text-white flex flex-col -mt-18">
+      <div className="w-full max-w-sm h-[80vh] bg-[#1f1f29] rounded-3xl p-6 text-white flex flex-col mx-auto">
         {showSummary ? (
           renderContent()
         ) : (
