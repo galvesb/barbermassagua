3874da71587f50eddd4d3c76d1e2f5e406e809b8
galvesb <guilherme.barrozzo@gmail.com>
2025-04-05 21:32:34 -0300

    ajustando tamanho

--- app/page.tsx
+++ app/page.tsx
@@ -321,7 +321,8 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-[#2a2a38] flex items-center justify-center p-4">
-      <div className="w-full max-w-sm h-[80vh] bg-[#1f1f29] rounded-3xl p-6 text-white flex flex-col -mt-20">
+      {/* altura */}
+      <div className="w-full max-w-sm h-[80vh] bg-[#1f1f29] rounded-3xl p-6 text-white flex flex-col -mt-18">
         {showSummary ? (
           renderContent()
         ) : (
